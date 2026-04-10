eb17cc4a4e747b4d3b9a6d834e7ead079c4beb4e
Samyak <samyaknayak77@gmail.com>
2026-04-10 10:59:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "I build data-driven web applications 🚀";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+window.onload = typeEffect;
